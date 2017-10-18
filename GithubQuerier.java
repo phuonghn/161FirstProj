864b8ae9a2083487f3a54e5d80dd72f1b6cd9078
phuonghn <hptnguyen@ucdavis.edu>
2017-10-17 18:40:39 -0700

    99% done

--- GithubQuerier.java
+++ GithubQuerier.java
@@ -2,22 +2,26 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
 
 import ecs161.querying.jira.util.Util;
 
 public class GithubQuerier {
 	
 	private static final String token =  "628c335763aba0e2d80361c0a1be3bc5f4f589f4";
+	private JSONObject jsonObj;
+	private JSONArray jsonArr;
 	
 	public static void main(String[] args) throws IOException {
 		GithubQuerier q = new GithubQuerier(args[0], args[1]);
+		q.query(args[2]);
 		
 		
-		System.out.println(q.query(args[1]));
+		//System.out.println(q.query(args[1]));
 		
-		GithubQuerier q2 = new GithubQuerier(args[2], args[3]);
+		//GithubQuerier q2 = new GithubQuerier(args[2], args[3]);
 		
-		System.out.println(q2.query(args[3]));
+		//System.out.println(q2.query(args[3]));
 		
 //		for (String a : args)
 //		{
@@ -28,7 +32,26 @@ public class GithubQuerier {
 	public String query(String userName) throws IOException {
 		int pullRequests = 0 ;
 		int mergeRequests = 0;
-	
+		//String user;
+		String login ;
+		String merge;
+		
+		for (int i = 0; i < this.jsonArr.length(); i++ ) {
+			login = this.jsonArr.optJSONObject(i).getJSONObject("user").getString("login");
+			//System.out.println(login);
+			//System.out.println(this.jsonArr.optJSONObject(i).get("merged_at").toString());
+			merge = this.jsonArr.optJSONObject(i).get("merged_at").toString();
+			if( login.equals(userName)) {
+				pullRequests = pullRequests +1;
+				if(!merge.equals("null")) {
+					mergeRequests++; 
+					
+				}
+				
+			}
+		}
+		System.out.println(mergeRequests);
+		System.out.println(pullRequests);
 		
 		return pullRequests + " " + mergeRequests;
 	}
@@ -37,9 +60,13 @@ public class GithubQuerier {
 		String projectName = name;
 		String accountName = account;
 		
-		URL url = new URL(baseURL + accountName + "/" + projectName + "?access_token" + token);
+		URL url = new URL(baseURL + accountName + "/" + projectName + "/pulls" + "?access_token=" + token +"&state=all");
 		JSONObject json = Util.queryURL(url);
-		
+		jsonArr= json.getJSONArray("root");
+//		
+//		System.out.println(json);
+//		System.out.println("separate");
+//		System.out.println(json.getJSONArray("root"));
 	}
 
 }
